4dd49066faab3e6a666a09094a22a30b30c2bebe
Shamsiddin <xamroqulovsh7@gmail.com>
2025-12-12 18:39:48 +0500

    Added otp generator random numbers

---
+++ src/utils/generators/otp.generator.js
@@ -0,0 +1,10 @@
+const otpGenerator = () => {
+    let randomNumbers = Number(Array.from({length: 6}, () => Math.ceil(Math.random() * 9)).join(""));
+    let otpTime = Date.now() + 120000;
+    return {
+        otp: randomNumbers,
+        otpTime
+    };
+};
+
+export default otpGenerator;
